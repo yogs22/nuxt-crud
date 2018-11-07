61cbeb4a6727c221bfc51e6ce552d59e9a2a2984
Yogi Prasetyawan <yogi.enjoyers22@gmail.com>
2018-11-07 20:26:43 +0700

    Store action

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,38 @@
 <template>
   <section class="container">
-	<h1>NuxJS Project</h1>
-	{{ countries }}
+	<h1 class="title">NuxJS Project</h1>
+	<ul class="panel">
+		<li class="panel-heading">Daftar Negara</li>
+		<li class="panel-block" v-for="country in countries" :key="country.id">
+			{{ country.name }}
+		</li>
+	</ul>
+	<form @submit.prevent="add(name)">
+		<div class="field">
+			<label class="label">Name</label>
+			<div class="control">
+				<input type="text" class="input" v-model="name">
+			</div>
+		</div>
+		<div class="control">
+			<button class="button is-link">Submit</button>
+		</div>
+	</form>
   </section>
 </template>
 
 <script>
-import axios from '~/plugins/axios'
+import { mapState, mapActions } from 'vuex'
 
 export default {
   computed: mapState([
-  		'countries'
-  	])
+  	'countries'
+  ]),
+  methods: {
+  	add(name) {
+  		this.$store.dispatch('store', name)
+  		this.name = ''
+  	}
+  }
 }
 </script>
--- store/index.js
+++ store/index.js
@@ -7,12 +7,19 @@ export const state = () => ({
 export const mutations = {
 	setData(state, items) {
 		state.countries = items
+	},
+	addData(state, item) {
+		state.countries.push(item)
 	}
 }
 
 export const actions = {
 	async nuxtServerInit({commit}) {
-		const result = await axios.get('negara')
+		const res = await axios.get('negara')
 		commit('setData', res.data)
+	},
+	async store({commit}, name) {
+		const res = await axios.post('negara', {name: name})
+		commit('addData', res.data)
 	}
 }
